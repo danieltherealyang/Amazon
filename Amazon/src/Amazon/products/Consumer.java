584c1cc12209bb5b173869b0e5e9e632ab3c3c3b
1831933 <1831933@20.8.20.203>
2020-02-25 14:18:12 -0800

    fix file stuff

--- Amazon/src/Amazon/products/Consumer.java
+++ Amazon/src/Amazon/products/Consumer.java
@@ -1,4 +1,8 @@
 package Amazon.products;
+import java.io.File; 
+import java.io.IOException; 
+import java.awt.image.BufferedImage; 
+import javax.imageio.ImageIO; 
 
 public class Consumer 
 {
@@ -14,6 +18,52 @@ public class Consumer
 		cprice=price;
 		ctarrif=tarrif;
 		//picture//
+		
+		int width = 963;    //width of the image 
+        int height = 640;   //height of the image 
+  
+        // For storing image in RAM 
+        BufferedImage image = null; 
+  
+        // READ IMAGE 
+        try
+        { 
+            File input_file = new File("Users/1831933/Downloads/here"); //image file path 
+  
+            /* create an object of BufferedImage type and pass 
+               as parameter the width,  height and image int 
+               type.TYPE_INT_ARGB means that we are representing 
+               the Alpha, Red, Green and Blue component of the 
+               image pixel using 8 bit integer value. */
+            image = new BufferedImage(width, height, 
+                                    BufferedImage.TYPE_INT_ARGB); 
+  
+             // Reading input file 
+            image = ImageIO.read(input_file); 
+  
+            System.out.println("Reading complete."); 
+        } 
+        catch(IOException e) 
+        { 
+            System.out.println("Error: "+e); 
+        } 
+  
+        // WRITE IMAGE 
+        
+        //try
+        //{ 
+            // Output file path 
+            //File output_file = new File("G:\\Out.jpg"); 
+  
+            // Writing to file taking type and path as 
+            //ImageIO.write(image, "jpg", output_file); 
+  
+            //System.out.println("Writing complete."); 
+        //} 
+
+	
+	
+	
 	}
 
 	
@@ -32,6 +82,12 @@ public class Consumer
 				
 				
 		};
+		
+		
+		
+		
+		
+		
 		
 		return con;
 	
